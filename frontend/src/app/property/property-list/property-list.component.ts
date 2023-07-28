f2654eefc3de79a70b2ceadecba73e7550d53ebb
Basem Moustafa <bmoustafa@berkeley.edu>
2023-07-27 21:49:39 -0700

    added styling and animations to propery card components

--- frontend/src/app/property/property-list/property-list.component.ts
+++ frontend/src/app/property/property-list/property-list.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
-export class PropertyListComponent {
+export class PropertyListComponent implements OnInit {
+  properties: Array<any> = [
+    {
+      Id: 1,
+      Name: 'House 1',
+      Type: 'House',
+      Price: 12000,
+    },
+    {
+      Id: 2,
+      Name: 'House 2',
+      Type: 'House',
+      Price: 15000,
+    },
+    {
+      Id: 3,
+      Name: 'House 3',
+      Type: 'House',
+      Price: 20000,
+    },
+    {
+      Id: 4,
+      Name: 'House 4',
+      Type: 'House',
+      Price: 25000,
+    },
+    {
+      Id: 5,
+      Name: 'House 5',
+      Type: 'House',
+      Price: 30000,
+    },
+    {
+      Id: 6,
+      Name: 'House 6',
+      Type: 'House',
+      Price: 35000,
+    },
+  ];
+  constructor() {}
 
+  ngOnInit(): void {}
 }
