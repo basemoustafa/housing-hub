fcb405012ac04f151b2f16885af89f3fd84b5a28
Basem Moustafa <bmoustafa@berkeley.edu>
2023-07-27 22:29:16 -0700

    changed default house image

--- frontend/src/app/property/property-list/property-list.component.ts
+++ frontend/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
 
 @Component({
   selector: 'app-property-list',
@@ -6,45 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent implements OnInit {
-  properties: Array<any> = [
-    {
-      Id: 1,
-      Name: 'House 1',
-      Type: 'House',
-      Price: 12000,
-    },
-    {
-      Id: 2,
-      Name: 'House 2',
-      Type: 'House',
-      Price: 15000,
-    },
-    {
-      Id: 3,
-      Name: 'House 3',
-      Type: 'House',
-      Price: 20000,
-    },
-    {
-      Id: 4,
-      Name: 'House 4',
-      Type: 'House',
-      Price: 25000,
-    },
-    {
-      Id: 5,
-      Name: 'House 5',
-      Type: 'House',
-      Price: 30000,
-    },
-    {
-      Id: 6,
-      Name: 'House 6',
-      Type: 'House',
-      Price: 35000,
-    },
-  ];
-  constructor() {}
+  properties!: any;
 
-  ngOnInit(): void {}
+  constructor(private houseingService: HousingService) {}
+
+  ngOnInit(): void {
+    this.houseingService.getAllProperties().subscribe(
+      (data) => {
+        this.properties = data;
+        console.log(data);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
