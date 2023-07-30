042fb0c2707d6add56f866f6d3a4391acc617ff9
Basem Moustafa <bmoustafa@berkeley.edu>
2023-07-30 16:29:59 -0700

    added IProperty Interface

--- frontend/src/app/services/housing.service.ts
+++ frontend/src/app/services/housing.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { IProperty } from '../property/IProperty.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +9,17 @@ import { HttpClient } from '@angular/common/http';
 export class HousingService {
   constructor(private http: HttpClient) {}
 
-  getAllProperties() {
-    return this.http.get('data/properties.json')
+  getAllProperties(): Observable<IProperty[]> {
+    return this.http.get('data/properties.json').pipe(
+      map((data: any) => {
+        const propertiesArr: Array<IProperty> = [];
+        for (const id in data) {
+          if (data.hasOwnProperty(id)) {
+            propertiesArr.push(data[id]);
+          }
+        }
+        return propertiesArr;
+      })
+    );
   }
 }
